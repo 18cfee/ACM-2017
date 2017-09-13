dcd3db5c0a23491c63cec388c67d8f8f8cfea80e
Carl Fee <cfee@localhost.localdomain>
2017-09-12 23:53:31 -0600

    e

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,23 +10,40 @@ public class Driver {
         Scanner in = new Scanner(new File("numbers.in"));
         PrintWriter out = new PrintWriter(new File("numbers.out"));
         while (in.hasNext()) {
-            int[] x = new int[4];
-            int[] y = new int[4];
-            boolean end = true;
-            for(int i = 0; i < 4; i++){
-                x[i] = in.nextInt();
-                if(x[i] != 0) end = false;
-                y[i] = in.nextInt();
-                if(y[i] != 0) end = false;
+            int a = in.nextInt();
+            if(a == 0) break;
+            HashSet<Integer> S = funcMult(a);
+            int tot = 0;
+            for(Integer I: S){
+                tot += I;
             }
-            if(end)break;
-            double minArea = area(x,y);
-
+            out.println(a == tot);
         }
         out.close();
     }
 
-    public static double area(int[] x, int[] y){
-        
+    public static HashSet<Integer> funcMult(int a){
+        int b = a;
+        HashSet<Integer> S = new HashSet<Integer>();
+        S.add(1);
+        for(int i = 2; i <= Math.sqrt(a); i++){
+            while(a%i == 0) {
+                HashSet<Integer> B = (HashSet<Integer>) S.clone();
+                for(Integer I: B){
+                    int mult = I*i;
+                    S.add(mult);
+                    //S.add(b/mult);
+                }
+                a=a/i;
+            }
+        }
+        HashSet<Integer> B = (HashSet<Integer>) S.clone();
+        for(Integer I: B){
+            int mult = I*a;
+            S.add(mult);
+            //S.add(b/mult);
+        }
+        S.remove(b);
+        return S;
     }
 }
