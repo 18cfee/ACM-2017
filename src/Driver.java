d6e60d4c941835e85cafedac6fb0a61141389f7b
Carl Fee <cfee@localhost.localdomain>
2017-09-12 19:30:35 -0600

    b

--- src/Driver.java
+++ src/Driver.java
@@ -7,25 +7,24 @@ public class Driver {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new File("numbers.in"));
         PrintWriter out = new PrintWriter(new File("numbers.out"));
-        int i2 = in.nextInt();
-        in.nextLine();
         while (in.hasNext()) {
-            String g = in.nextLine();
-            String f = in.nextLine();
-            String create = "";
-            for (int i = 0; i < f.length(); i++) {
-                int a1 = ((int) g.charAt(i) - 1);
-                int b1 = (int) f.charAt(i) + 1;
-                if (a1 == 31) a1 = 126;
-                if (b1 == 127) b1 = 32;
-                String a = String.valueOf((char) (a1));
-                String b = String.valueOf((char) (b1));
-                create += a + b;
+            int[] x = new int[4];
+            int[] y = new int[4];
+            boolean end = true;
+            for(int i = 0; i < 4; i++){
+                x[i] = in.nextInt();
+                if(x[i] != 0) end = false;
+                y[i] = in.nextInt();
+                if(y[i] != 0) end = false;
             }
-            if (f.length() < g.length())
-                create += (g.charAt(g.length() - 1) + 1); // this does not work or have logic to roll around
-            out.println(create);
+            if(end)break;
+            double minArea = area(x,y);
+
         }
         out.close();
+    }
+
+    public static double area(int[] x, int[] y){
+        
     }
 }
